53f42854c204f5953ddda97f2b98607aaece4b44
Sehar Malik <seharmalik241@gmail.com>
2020-09-23 17:46:47 -0500

    Modified Adder method to perform subtraction

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,15 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide 3 integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        
+	if (args[0] == '-'){
+	return (-1*(Integer.valueOf(args[1] + Integer.valueOf(args[2]));
+	}
+	return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
     }
 }
